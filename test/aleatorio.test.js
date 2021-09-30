b0446410bb850fe14ea1d40dd28e763c36eaee32
Guilherme Camargo <guilhermecamargo57@hotmail.com>
2021-09-29 21:54:50 -0300

    Test the end of the range is a negative value - '4,-2'

--- test/aleatorio.test.js
+++ test/aleatorio.test.js
@@ -6,4 +6,8 @@ describe("Aleatorio", () => {
   test("test start of range is a negative value", () => {
     expect(aleatorio.gerarNumeroAleatorio(-1, 5)).toBe(-1);
   });
+
+  test("test the end of the range is a negative value", () => {
+    expect(aleatorio.gerarNumeroAleatorio(4, -2)).toBe(-1);
+  });
 });
