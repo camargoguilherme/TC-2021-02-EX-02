4bc24c680e918bb03b683ed3cff240e271f4bfab
Guilherme Camargo <guilhermecamargo57@hotmail.com>
2021-09-29 21:29:11 -0300

    Test a unordened array '[1, 99, 3, -5, 8]'

--- test/extremos.test.js
+++ test/extremos.test.js
@@ -2,4 +2,22 @@ const Extremos = require("../lib/extremos");
 
 const extremos = new Extremos();
 
-describe("Extremos", () => {});
+describe("Extremos", () => {
+  test("test a small array", () => {
+    expect(extremos.acharExtremos([1, 2, 3, 4, 5, 6])).toMatchObject({
+      menor: 1,
+      indiceMenor: 0,
+      maior: 6,
+      indiceMaior: 5,
+    });
+  });
+
+  test("test a unordened array", () => {
+    expect(extremos.acharExtremos([1, 99, 3, -5, 8])).toMatchObject({
+      menor: -5,
+      indiceMenor: 3,
+      maior: 99,
+      indiceMaior: 1,
+    });
+  });
+});
