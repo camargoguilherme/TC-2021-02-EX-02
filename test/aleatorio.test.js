c6a56b93f679d13f6eee26803f6cb384892d4835
Guilherme Camargo <guilhermecamargo57@hotmail.com>
2021-09-29 21:53:08 -0300

    Test start of range is a negative value - '-1,5'

--- test/aleatorio.test.js
+++ test/aleatorio.test.js
@@ -2,4 +2,8 @@ const Aleatorio = require("../lib/aleatorio");
 
 const aleatorio = new Aleatorio();
 
-describe("Aleatorio", () => {});
+describe("Aleatorio", () => {
+  test("test start of range is a negative value", () => {
+    expect(aleatorio.gerarNumeroAleatorio(-1, 5)).toBe(-1);
+  });
+});
